a24c793eacb083d28534e7741db25922170c1d13
rahul <rahulsalinskee@gmail.com>
2026-01-13 17:55:21 +0530

    Added Application Error model and Response DTO. Also, updated .gitignore file for reading LogConfiguration class file.

---
+++ EmployeeApplication.Model/DTOs/ResponseDTOS/ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeApplication.Model.DTOs.ResponseDTOS
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+
+        public bool IsSuccess { get; set; } = false;
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ EmployeeApplication.Model/Models/ApplicationError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeApplication.Model.Models
+{
+    public class ApplicationError
+    {
+        public Guid ID { get; set; }
+
+        public DateTime When { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
