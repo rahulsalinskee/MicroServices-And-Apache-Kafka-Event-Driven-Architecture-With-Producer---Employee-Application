2827cbd009fda07d63d898f8cbeade1abb17a837
rahul <rahulsalinskee@gmail.com>
2026-01-19 15:58:00 +0530

    Have done code refactor. Still bootstarpservers is not moved to appsettings.json

--- EmployeeApplication.Repository/Repository/Implementations/EmployeeApplicationImplementation.cs
+++ EmployeeApplication.Repository/Repository/Implementations/EmployeeApplicationImplementation.cs
@@ -6,17 +6,26 @@ using EmployeeApplication.Model.EmployeeMapping;
 using EmployeeApplication.Model.Models;
 using EmployeeApplication.Repository.Repository.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 using System.Text.Json;
 
 namespace EmployeeApplication.Repository.Repository.Implementations
 {
     public class EmployeeApplicationImplementation : IEmployeeApplicationService
     {
-        private readonly EmployeeApplicationDbContext _employeeApplicationDbContext;
+        private const string ADD_EMPLOYEE_TOPIC = "add-employee-topic";
+        private const string UPDATE_EMPLOYEE_TOPIC = "update-employee-topic";
+        private const string DELETE_EMPLOYEE_TOPIC = "delete-employee-topic";
+        private const string RESOURCE_NAME = "EmployeeApplication.Repository.appsettings.json";
 
-        public EmployeeApplicationImplementation(EmployeeApplicationDbContext employeeApplicationDbContext)
+        private readonly EmployeeApplicationDbContext _employeeApplicationDbContext;
+        private readonly IConfiguration _configuration;
+
+        public EmployeeApplicationImplementation(EmployeeApplicationDbContext employeeApplicationDbContext, IConfiguration configuration)
         {
             this._employeeApplicationDbContext = employeeApplicationDbContext;
+            this._configuration = configuration;
         }
 
         public async Task<ResponseDto> CreateEmployeeAsync(AddEmployeeDto addEmployeeDto)
@@ -72,22 +81,8 @@ namespace EmployeeApplication.Repository.Repository.Implementations
             await this._employeeApplicationDbContext.SaveChangesAsync();
             var addedEmployeeDto = employee.ConvertEmployeeToEmployeeDtoExtension();
 
-            var message = new Message<string, string>()
-            {
-                Key = addedEmployeeDto.Id.ToString(),
-                Value = JsonSerializer.Serialize(addedEmployeeDto),
-            };
-
-            ProducerConfig producerConfiguration = new()
-            {
-                BootstrapServers = "localhost:9094",
-                Acks = Acks.All,
-            };
-
-            IProducer<string, string> producerBuilder = new ProducerBuilder<string, string>(producerConfiguration).Build();
-
-            await producerBuilder.ProduceAsync(topic: "employee-topic", message: message);
-            producerBuilder.Dispose();
+            var message = GetMessageConfiguration(messageKey: addedEmployeeDto.Id.ToString(), employeeObject: addedEmployeeDto);
+            await ProducerConfigurationAsync(topicName: ADD_EMPLOYEE_TOPIC, message: message);
 
             return new ResponseDto()
             {
@@ -147,6 +142,9 @@ namespace EmployeeApplication.Repository.Repository.Implementations
                 LastName = employee.LastName,
             };
 
+            var message = GetMessageConfiguration(messageKey: employeeDto.Id.ToString(), employeeObject: employeeDto);
+            await ProducerConfigurationAsync(topicName: DELETE_EMPLOYEE_TOPIC, message: message);
+
             return new ResponseDto()
             {
                 Result = employeeDto,
@@ -190,7 +188,7 @@ namespace EmployeeApplication.Repository.Repository.Implementations
         public async Task<ResponseDto> GetEmployeesAsync()
         {
             var employees = await this._employeeApplicationDbContext.Employees.ToListAsync();
-            
+
             IList<EmployeeDto> employeesDto = [];
 
             if (employees is null || !employees.Any())
@@ -307,12 +305,56 @@ namespace EmployeeApplication.Repository.Repository.Implementations
 
             var updatedEmployeeDto = employee.ConvertEmployeeToEmployeeDtoExtension();
 
+            var message = GetMessageConfiguration(messageKey: updatedEmployeeDto.Id.ToString(), employeeObject: updatedEmployeeDto);
+            await ProducerConfigurationAsync(topicName: UPDATE_EMPLOYEE_TOPIC, message: message);
+
             return new ResponseDto()
             {
                 Result = updatedEmployeeDto,
                 IsSuccess = true,
                 Message = "Employee updated successfully!",
             };
+        }
+
+        private static Message<string, string> GetMessageConfiguration(string messageKey, object employeeObject)
+        {
+            var message = new Message<string, string>()
+            {
+                Key = messageKey,
+                Value = JsonSerializer.Serialize(employeeObject),
+            };
+
+            return message;
+        }
+
+        private async Task ProducerConfigurationAsync(string topicName, Message<string, string> message)
+        {
+            string bootstrapServerValue = GetKafkaConfigurationValue();
+            ProducerConfig producerConfiguration = new()
+            {
+                //BootstrapServers = bootstrapServerValue,
+                BootstrapServers = "localhost:9094",
+                Acks = Acks.All,
+            };
+
+            IProducer<string, string> producerBuilder = new ProducerBuilder<string, string>(producerConfiguration).Build();
+
+            await producerBuilder.ProduceAsync(topic: topicName, message: message);
+            producerBuilder.Dispose();
+        }
+
+        private string GetKafkaConfigurationValue()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using var manifestResourceStream = assembly.GetManifestResourceStream(name: RESOURCE_NAME);
+
+            if (manifestResourceStream is not null)
+            {
+                //builder.AddJsonStream(stream: manifestResourceStream);
+            }
+
+            return _configuration.GetSection(key: "Kafka:BootstrapServers").Value ?? string.Empty;
         }
     }
 }
